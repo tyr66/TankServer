90dcbaa24997ec61f35d8ba14be3a657fddda903
tyr66 <1847009824tan@gmail.com>
2022-05-21 15:56:10 +0800

    Add:初始化

---
+++ main.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TankServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 1234;
+            IPEndPoint address = new IPEndPoint(ip,port);
+            serverSocket.Bind(address);
+
+            serverSocket.Listen(1024);
+
+            Console.WriteLine("Server start to listen");
+
+            while(true)
+            {
+                Socket client = serverSocket.Accept();
+                Console.WriteLine("Receive to client");
+
+                byte[] recvBuf = new byte[1024];
+                int count = client.Receive(recvBuf);
+                string str = System.Text.Encoding.UTF8.GetString(recvBuf,0,count);
+                Console.WriteLine(str);
+
+                byte[] sendBuf = System.Text.Encoding.Default.GetBytes("server echo :"+str);
+                client.Send(sendBuf);
+            }
+        }
+    }
+}
